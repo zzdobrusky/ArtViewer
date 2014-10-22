36c77c78a91b1cffe8a6e547e7e44c3790e54f73
zbynek <zzdobrusky@gmail.com>
2014-10-21 22:08:47 -0600

    Initial commit

---
+++ app/src/main/java/edu/utah/cs4962/artviewer/ArtActivity.java
@@ -0,0 +1,17 @@
+package edu.utah.cs4962.artviewer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class ArtActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_art);
+    }
+}
